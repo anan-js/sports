766e01af25ce518c37de70494d12b483ee0b2fba
Wu AnAn <2461051635@qq.com>
2021-04-17 19:22:37 +0800

    4-17
    Signed-off-by: Wu AnAn <2461051635@qq.com>

--- App.vue
+++ App.vue
@@ -1,10 +1,14 @@
 <script>
 	export default {
+    globalData:{
+      view:25,
+      width:375
+    },
 		onLaunch: function() {
 			console.log('App Launch')
-      // #ifdef MP-WEIXIN
-      console.log(uni.getSystemInfoSync())
-      // #endif
+      const view=uni.getSystemInfoSync()
+      console.log(view)
+      this.globalData.width=view.windowWidth
 		},
 		onShow: function() {
 			console.log('App Show')
@@ -15,6 +19,17 @@
 	}
 </script>
 
-<style>
+<style lang="scss" >
 	/*每个页面公共css */
+  page{
+    background-color: #C8C7CC;
+  }
+ .c1{
+   text-overflow: ellipsis;
+   overflow: hidden;
+   display: -webkit-box;
+       -webkit-line-clamp: 1; /* 限制在一个块元素显示的文本的行数 */
+       -webkit-box-orient: vertical; /* 垂直排列 */
+       word-break: break-all;  /* 内容自动换行 */
+ }
 </style>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,24 +1,91 @@
 <template>
-	<view>
-		home
-    <navigator url="../index/index">index</navigator>
-	</view>
+  <view>
+    <!-- 状态栏 -->
+  <view class="status_bar" ></view>
+    <homeSwiper>
+      <swiper-item v-for="i in 4">
+        <view class="swiper-item">{{i}}</view>
+      </swiper-item>
+    </homeSwiper>
+    <gird>
+      <gridItem v-for="i in icons" >
+        <view class="img_box" :style="{width:width+'px',height:width+'px'}">
+          <image class="image" :src="i" mode="aspectFit" ></image>
+        </view>
+        <text class="c1">{{js(i)}}</text>
+      </gridItem>
+    </gird>
+    <view class="twoS">
+      <homeSwiper :height="110" >
+        <swiper-item v-for="i in 2" >
+         <homeGrid>
+           <homeGridItem v-for="i in 4">
+             <view class="bl">
+               <image class="image" src="../../static/icons/其他.png" mode="aspectFit"></image>
+             </view>
+             <text>{{i}}</text>
+           </homeGridItem>
+         </homeGrid>
+        </swiper-item>
+      </homeSwiper>
+    </view>
+  <homeBody>
+    
+  </homeBody>
+  </view>
 </template>
 
 <script>
+  import pngs from "./icons.js"
 	export default {
 		data() {
 			return {
-				
+        icons:[],
+        width:75
 			}
 		},
+    onLoad() {
+      console.log(pngs,this.$data.icons)
+      this.$data.icons.splice(0,0,...pngs)
+      this.$data.width=(getApp().globalData.width/5)*0.5
+      console.log(this.$data.width,getApp().globalData)
+    },
 		methods: {
-			
+			js(i){
+        return i.slice(12).slice(0,i.slice(12).search(/\./))
+      }
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-
-
+.status_bar {
+      height: var(--status-bar-height);
+      width: 100%;
+  }
+.img_box{
+  background: blue;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.image{
+    width: 75%;
+    height: 75%;
+}
+.twoS{
+  margin: 5px 0;
+  box-sizing: border-box;
+  padding-top: 10px ;
+  background-color: white;
+}
+.bl{
+  width: 90%;
+  height: 75%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: blue;
+}
 </style>
